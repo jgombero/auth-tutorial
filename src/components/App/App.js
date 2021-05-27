d9f8a9088699e50f15dd07752fedb48156ec4bc9
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-27 15:08:53 -0700

    Render components with react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Preferences from "../Preferences/Preferences";
 
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
