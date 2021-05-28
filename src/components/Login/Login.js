b68b560b5a97e846f310cec0266ce7aa4606ae8e
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-28 09:16:25 -0700

    Use fetch api to get token from back end

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
 import "./Login.css";
 
-const Login = () => {
+const loginUser = async (credentials) => {
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+};
+
+const Login = ({ setToken }) => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password,
+    });
+    setToken(token);
+  };
+
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={(e) => setUsername(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -23,3 +50,7 @@ const Login = () => {
 };
 
 export default Login;
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
