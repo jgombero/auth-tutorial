6b9f2acd1d33a682641481b7ece3f3172b934ac2
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-28 08:49:54 -0700

    Render Login component when no token set

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
-import "./App.css";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 import Dashboard from "../Dashboard/Dashboard";
 import Preferences from "../Preferences/Preferences";
+import Login from "../Login/Login";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
